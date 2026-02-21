19fec9a48e74368b0aaf79674287f2f8adfd7639
LilYa <LilYa75310@gmail.com>
2026-02-22 00:58:52 +0200

    added gpio_core.sv

--- sources/rtl/gpio_core.sv
+++ sources/rtl/gpio_core.sv
@@ -5,31 +5,194 @@ module axi_gpio #(
   parameter int AXI_DATA_WIDTH = 32,
   parameter int GPIO_WIDTH = 32
 )(  
-
-//   //GPIO interface signals
-//   //Channel 1
-//   input  logic [GPIO_WIDTH-1:0]            GPIO_IO_I,
-//   output logic [GPIO_WIDTH-1:0]            GPIO_IO_O,
-//   output logic [GPIO_WIDTH-1:0]            GPIO_IO_T,
-//   //Channel 2
-//   input  logic [GPIO_WIDTH-1:0]            GPIO2_IO_I,
-//   output logic [GPIO_WIDTH-1:0]            GPIO2_IO_O,
-//   output logic [GPIO_WIDTH-1:0]            GPIO2_IO_T,
+  //Global sys signals 
+  input  logic                             clk_i,
+  input  logic                             reset_n,
+  //GPIO write interface signals
+  input  logic                             wr_en_i,
+  input  logic [AXI_ADDR_WIDTH-1:0]        wr_addr_i,
+  input  logic [AXI_DATA_WIDTH/8-1:0]      wr_strb_i,
+  input  logic [AXI_DATA_WIDTH-1:0]        wr_data_i,
+  //GPIO read interface signals
+  output logic                             rd_done_o,
+  output logic [AXI_DATA_WIDTH-1:0]        rd_data_o,
+  output logic [1:0]                       rd_err_o,
+  input  logic                             rd_en_i,
+  input  logic [AXI_ADDR_WIDTH-1:0]        rd_addr_i,
 
   //GPIO inout pins
   //Channel 1
-  inout logic [GPIO_WIDTH-1:0]             GPIO_IO,
+  inout  logic [GPIO_WIDTH-1:0]            gpio_io
   //Channel 2
-  inout logic [GPIO_WIDTH-1:0]             GPIO2_IO
+  //inout  logic [GPIO_WIDTH-1:0]            gpio2_io
 );
-
-  // //GPIO interface signals
-  // logic [GPIO_WIDTH-1:0]            read_reg_in;
+  
   // //Channel 1
-  // logic [GPIO_WIDTH-1:0]            gpio_tri;
-  // logic [GPIO_WIDTH-1:0]            gpio_data;
+  logic [GPIO_WIDTH-1:0]            gpio_tri;
+  logic [GPIO_WIDTH-1:0]            gpio_data;
+  logic [GPIO_WIDTH-1:0]            gpio_data_in;
+  logic [AXI_ADDR_WIDTH-1:0]        rd_addr_q;
   // //Channel 2
   // logic [GPIO_WIDTH-1:0]            gpio2_tri;
   // logic [GPIO_WIDTH-1:0]            gpio2_data;
+
+  //assign gpio_io = gpio_tri ? gpio_data : 'z;
+  assign gpio_io = gpio_tri ? gpio_data : {GPIO_WIDTH{1'bz}};
+
+  // genvar i;
+  // generate
+  //   for (i = 0; i < GPIO_WIDTH; i++) begin
+  //     assign gpio_io[i] = gpio_tri[i] ? gpio_data[i] : 1'bz;
+  //   end
+  // endgenerate
+
+  assign gpio_data_in = gpio_io;
+  // що буде якщо  gpio_size < axi_size?
+  always_ff @(posedge clk_i or negedge reset_n) begin : write_logic
+    if(!reset_n) begin
+      gpio_data <= '0;
+      gpio_tri <= '1;
+    end else begin
+      if(wr_en_i) begin
+        case(wr_addr_i[3:0]) begin
+          4'h0: begin
+            for (int i = 0; i < AXI_DATA_WIDTH/8; i++) begin
+              if ((i*8) < GPIO_WIDTH) begin
+                if(wr_strb_i[i]) begin
+                  gpio_data[i*8 +: 8] <= wr_data_i[i*8 +: 8];
+                end
+              end
+            end
+          end
+
+          4'h4: begin
+            for (int i = 0; i < AXI_DATA_WIDTH/8; i++) begin
+              if ((i*8) < GPIO_WIDTH) begin
+                if(wr_strb_i[i]) begin
+                  gpio_tri[i*8 +: 8] <= wr_data_i[i*8 +: 8];
+                end
+              end
+            end
+          end
+
+          default: ;
+        end
+      end
+    end
+  end
+
+  //read interface
+  typedef enum logic [1:0] {
+    RD_IDLE,
+    RD_BUSY,
+    RD_DONE
+  } read_state_t;
+  
+  read_state_t rd_state;
+  read_state_t rd_state_next;
+
+  logic [1:0] counter;
+  
+  // State register
+  always_ff @(posedge clk_i or negedge reset_n) begin : fsm_read_state_reg
+    if(!reset_n) begin
+      rd_state <= RD_IDLE;
+    end else begin
+      rd_state <= rd_state_next;
+    end
+  end
+  
+  // Counter
+  always_ff @(posedge clk_i or negedge reset_n) begin : fsm_read_state_reg
+    if(!reset_n) begin
+      counter <= '0;
+    end else begin
+      if(rd_state == RD_BUSY) begin
+        if(counter == 2'd1) begin
+          counter <= 2'd0;
+        end else begin
+          counter <= counter + 1'b1;
+        end
+      end else begin
+        counter <= '0;
+      end
+    end
+  end
+  
+  always_ff @(posedge clk_i or negedge reset_n) begin
+    if(!reset_n)
+      rd_addr_q <= '0;
+    else if(rd_state == RD_IDLE && rd_en_i)
+      rd_addr_q <= rd_addr_i;
+  end
+
+  // Next-state logic
+  always_comb begin : fsm_read_next_state_logic
+    //rd_state_next = 2'bx;
+
+    case (rd_state)
+      RD_IDLE: begin
+        if(rd_en_i) begin
+          rd_state_next = RD_BUSY;
+        end else begin
+          rd_state_next = RD_IDLE;
+        end
+      end
+
+      RD_BUSY: begin
+        if(counter == 2'd1) begin
+          rd_state_next = RD_DONE;
+        end else begin
+          rd_state_next = BUSY;
+        end
+      end
+
+      RD_DONE: begin
+        rd_state_next = RD_IDLE;
+      end
+
+      default: rd_state_next = RD_IDLE;
+    endcase
+  end
+
+  // Output register
+  always_ff @(posedge clk_i or negedge reset_n) begin : fsm_out_reg
+    if(!reset_n) begin
+      rd_data_o <= '0;
+      rd_done_o <= '0;
+      rd_err_o <= '0;
+    end else begin
+      rd_data_o <= 1'b0;
+      rd_err_o <= 2'b00;
+
+      case (rd_state_next)
+        RD_IDLE: ;
+
+        RD_BUSY: ;
+
+        RD_DONE: begin
+          rd_done_o <= 1'b1;
+          //case (rd_addr_i[3:0])
+          case (rd_addr_q[3:0])
+            4'h0: begin
+              rd_data_o <= gpio_data_in;
+              //rd_data_o <= {{(AXI_DATA_WIDTH-GPIO_WIDTH){1'b0}}, gpio_data_in};
+            end
+
+            4'h4: begin
+              rd_data_o <= gpio_tri;
+              //rd_data_o <= {{(AXI_DATA_WIDTH-GPIO_WIDTH){1'b1}}, gpio_tri};
+            end
+
+            default: begin
+              rd_data_o <= '0;
+              rd_err_o <= 2'b10;
+            end
+          endcase
+        end
+      endcase
+    end
+  end
+
 
 endmodule
