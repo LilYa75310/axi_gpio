5b39d61103866b6dcf67d2973e58a20cf953b9ae
LilYa <LilYa75310@gmail.com>
2026-02-22 21:39:58 +0200

    updated axi_gpio.sv

--- sources/rtl/axi_gpio.sv
+++ sources/rtl/axi_gpio.sv
@@ -6,155 +6,115 @@ module axi_gpio #(
   parameter int GPIO_WIDTH = 32
 )(  
   //Global sys signals 
-  input  logic                             S_AXI_ACLK,
-  input  logic                             S_AXI_ARESETN,
+  input  logic                             s_axi_aclk_i,
+  input  logic                             s_axi_areset_n,
 
   //Axi write address channel
-  input  logic [AXI_ADDR_WIDTH-1:0]        S_AXI_AWADDR_I,
-  input  logic                             S_AXI_AWVALID_I,
-  output logic                             S_AXI_AWREADY_O,
-
+  input  logic [AXI_ADDR_WIDTH-1:0]        s_axi_awaddr_i,
+  input  logic                             s_axi_awvalid_i,
+  output logic                             s_axi_awready_o,
   //Axi write data channel
-  input  logic [AXI_DATA_WIDTH-1:0]        S_AXI_WDATA_I,
-  input  logic [AXI_DATA_WIDTH/8-1:0]      S_AXI_WSTB_I,
-  input  logic                             S_AXI_WVALID_I,
-  output logic                             S_AXI_WREADY_O,
-
+  input  logic [AXI_DATA_WIDTH-1:0]        s_axi_wdata_i,
+  input  logic [AXI_DATA_WIDTH/8-1:0]      s_axi_wstb_i,
+  input  logic                             s_axi_wvalid_i,
+  output logic                             s_axi_wready_o,
   //Axi write response channel
-  output logic [1:0]                       S_AXI_BRESP_O,
-  output logic                             S_AXI_BVALID_O,
-  input  logic                             S_AXI_BREADY_I,
-
+  output logic [1:0]                       s_axi_bresp_o,
+  output logic                             s_axi_bvalid_o,
+  input  logic                             s_axi_bready_i,
   //Axi read address channel
-  input  logic [AXI_ADDR_WIDTH-1:0]        S_AXI_ARADDR_I,
-  input  logic                             S_AXI_ARVALID_I,
-  output logic                             S_AXI_ARREADY_O,
-  
+  input  logic [AXI_ADDR_WIDTH-1:0]        s_axi_araddr_i,
+  input  logic                             s_axi_arvalid_i,
+  output logic                             s_axi_arready_o,
   //Axi read data channel
-  output logic [AXI_DATA_WIDTH-1:0]        S_AXI_RDATA_O,
-  output logic [1:0]                       S_AXI_RRESP_O,
-  output logic                             S_AXI_RVALID_O,
-  input  logic                             S_AXI_RREADY_I,
-
-  //GPIO interface signals
-  output logic                             DATA_RECEIVED_O,
-  output logic [AXI_ADDR_WIDTH-1:0]        AW_DATA_REG_O,
-  output logic [AXI_DATA_WIDTH-1:0]        W_DATA_REG_O,
-  output logic [AXI_DATA_WIDTH/8-1:0]      W_STROBE_O,
-  output logic [AXI_ADDR_WIDTH-1:0]        READ_ADDR_O,
-  input  logic [AXI_DATA_WIDTH-1:0]        GPIO_DATA_I                    
+  output logic [AXI_DATA_WIDTH-1:0]        s_axi_rdata_o,
+  output logic [1:0]                       s_axi_rresp_o,
+  output logic                             s_axi_rvalid_o,
+  input  logic                             s_axi_rready_i,
+  //GPIO write interface signals
+  output logic [AXI_ADDR_WIDTH-1:0]        wr_addr_o,
+  output logic [AXI_DATA_WIDTH-1:0]        wr_data_o,
+  output logic [AXI_DATA_WIDTH/8-1:0]      wr_strb_o,
+  output logic                             wr_en_o,
+  //GPIO read interface signals
+  input  logic                             rd_done_i,
+  input  logic [AXI_DATA_WIDTH-1:0]        rd_data_i,
+  input  logic [1:0]                       rd_err_i,
+  output logic                             rd_en_o,
+  output logic [AXI_ADDR_WIDTH-1:0]        rd_addr_o
 );
-  //AW
-  logic                             axi_awready;
-  
-  //assign outputs to top level signals
-  assign S_AXI_AWREADY_O = axi_awready;
-  
-  //W
-  logic                             axi_wready;
-  assign S_AXI_WREADY_O = axi_wready;
-
-  //B
-  logic [1:0]                       axi_bresp;
-  logic                             axi_bvalid;
-  assign S_AXI_BRESP_O = axi_bresp;
-  assign S_AXI_BVALID_O = axi_bvalid;
-
-  //AR
-  logic                             axi_arready;
-  assign S_AXI_ARREADY_O = axi_arready;
-  
-  //R
-  logic [AXI_DATA_WIDTH-1:0]        axi_rdata;
-  logic [1:0]                       axi_rresp;
-  logic                             axi_rvalid;
-  assign S_AXI_RDATA_O = axi_rdata;
-  assign S_AXI_RRESP_O = axi_rresp;
-  assign S_AXI_RVALID_O = axi_rvalid;
-
-  //GPIO interface
-  logic                             data_received;
-  logic [AXI_ADDR_WIDTH-1:0]        aw_data_reg;
-  logic [AXI_DATA_WIDTH-1:0]        w_data_reg;
-  logic [AXI_DATA_WIDTH/8-1:0]      w_strobe;
-  logic [AXI_ADDR_WIDTH-1:0]        read_addr;
-  
-  assign DATA_RECEIVED_O = data_received;
-  assign AW_DATA_REG_O = aw_data_reg;
-  assign W_DATA_REG_O = w_data_reg;
-  assign W_STROBE_O = w_strobe;
-  assign READ_ADDR_O = read_addr;
   
   // internal signals
-  logic aw_done;
-  logic w_done;
+  logic aw_shake_q;
+  logic w_shake_q;
   logic aw_shake;
   logic w_shake;
   
-  assign aw_shake = S_AXI_AWVALID_I && axi_awready;
-  assign w_shake = S_AXI_WVALID_I && axi_wready;
+  assign aw_shake = s_axi_awvalid_i && s_axi_awready_o;
+  assign w_shake = s_axi_wvalid_i && s_axi_wready_o;
+
 
   // ======================================
   // AXI WRITE
   // ======================================
   //AW handshake
-  always_ff @(posedge S_AXI_ACLK or negedge S_AXI_ARESETN) begin : AW_handshake
-    if(!S_AXI_ARESETN) begin
-      axi_awready <= 1'b1;
-      aw_data_reg <= '0;
+  always_ff @(posedge s_axi_aclk_i or negedge s_axi_areset_n) begin : AW_handshake
+    if(!s_axi_areset_n) begin
+      s_axi_awready_o <= 1'b1;
+      wr_addr_o <= '0;
     end else begin
-      if(S_AXI_AWVALID_I && axi_awready) begin
-        axi_awready <= 1'b0;
-        aw_data_reg <= S_AXI_AWADDR_I;
-      end else if (axi_bvalid && S_AXI_BREADY_I) begin
-        axi_awready <= 1'b1;
+      if(aw_shake) begin
+        s_axi_awready_o <= 1'b0;
+        wr_addr_o <= s_axi_awaddr_i;
+      end else if (s_axi_bvalid_o && s_axi_bready_i) begin
+        s_axi_awready_o <= 1'b1;
       end
     end
   end
 
   //W handshake
-  always_ff @(posedge S_AXI_ACLK or negedge S_AXI_ARESETN) begin : W_handshake
-    if(!S_AXI_ARESETN) begin
-      axi_wready <= 1'b1;
-      w_data_reg <= '0;
-      w_strobe <= '0;
-      data_received <= 1'b0;
+  always_ff @(posedge s_axi_aclk_i or negedge s_axi_areset_n) begin : W_handshake
+    if(!s_axi_areset_n) begin
+      s_axi_wready_o <= 1'b1;
+      wr_data_o <= '0;
+      wr_strb_o <= '0;
     end else begin
-      if(S_AXI_WVALID_I && axi_wready) begin
-        axi_wready <= 1'b0;
-        w_data_reg <= S_AXI_WDATA_I;
-        w_strobe <= S_AXI_WSTB_I;
-        data_received <= 1'b1;
-      end else if (axi_bvalid && S_AXI_BREADY_I) begin
-        axi_wready <= 1'b1;
-        data_received <= 1'b0;
+      if(w_shake) begin
+        s_axi_wready_o <= 1'b0;
+        wr_data_o <= s_axi_wdata_i;
+        wr_strb_o <= s_axi_wstb_i;
+      end else if (s_axi_bvalid_o && s_axi_bready_i) begin
+        s_axi_wready_o <= 1'b1;
       end
     end
   end
   
   // B response
-  always_ff @(posedge S_AXI_ACLK or negedge S_AXI_ARESETN) begin : B_response
-    if(!S_AXI_ARESETN) begin
-      aw_done <= 1'b0;
-      w_done <= 1'b0;
-      axi_bvalid <= 1'b0;
-      axi_bresp <= 2'b00;
+  always_ff @(posedge s_axi_aclk_i or negedge s_axi_areset_n) begin : B_response
+    if(!s_axi_areset_n) begin
+      aw_shake_q <= 1'b0;
+      w_shake_q <= 1'b0;
+      s_axi_bvalid_o <= 1'b0;
+      s_axi_bresp_o <= 2'b00;
+      wr_en_o <= 1'b0;
     end else begin
       if(aw_shake) begin
-        aw_done <= 1'b1;
+        aw_shake_q <= 1'b1;
       end
 
       if(w_shake) begin
-        w_done <= 1'b1;
+        w_shake_q <= 1'b1;
       end
 
-      if((aw_done || aw_shake) && (w_done || w_shake) && !axi_bvalid) begin
-        axi_bvalid <= 1'b1;
-        axi_bresp <= 2'b00;
-      end else if(axi_bvalid && S_AXI_BREADY_I) begin
-        axi_bvalid <= 1'b0;
-        aw_done <= 1'b0;
-        w_done <= 1'b0;
+      if((aw_shake_q || aw_shake) && (w_shake_q || w_shake) && !s_axi_bvalid_o) begin
+        s_axi_bvalid_o <= 1'b1;
+        s_axi_bresp_o <= 2'b00;
+        wr_en_o <= 1'b1;
+      end else if(s_axi_bvalid_o && s_axi_bready_i) begin
+        s_axi_bvalid_o <= 1'b0;
+        aw_shake_q <= 1'b0;
+        w_shake_q <= 1'b0;
+        wr_en_o <= 1'b0;
       end
     end
   end
@@ -164,112 +124,99 @@ module axi_gpio #(
   // AXI READ
   // ======================================
 
-  always_ff @(posedge S_AXI_ACLK or negedge S_AXI_ARESETN) begin : AR_handshake
-    if(!S_AXI_ARESETN) begin
-      axi_arready <= 1'b1;
-      read_addr <= '0;
+  typedef enum logic [1:0] {
+    RD_IDLE,
+    RD_ADDR,
+    RD_WAIT_DONE,
+    RD_WRITE_AXI_SIG
+  } read_state_t;
+  
+  read_state_t rd_state;
+  read_state_t rd_state_next;
+  
+  // State register
+  always_ff @(posedge s_axi_aclk_i or negedge s_axi_areset_n) begin : fsm_read_state_reg
+    if(!s_axi_areset_n) begin
+      rd_state <= RD_IDLE;
     end else begin
-      if(axi_arready && S_AXI_ARVALID_I) begin
-        axi_arready <= 1'b0;
-        read_addr <=  S_AXI_ARADDR_I;
-      end else if(axi_rvalid && S_AXI_RREADY_I) begin
-        axi_arready <= 1'b1;
-      end
+      rd_state <= rd_state_next;
     end
   end
 
-  always_ff @(posedge S_AXI_ACLK or negedge S_AXI_ARESETN) begin : R_handshake
-    if(!S_AXI_ARESETN) begin
-      axi_rvalid <= 1'b0;
-      //axi_rdata <= '0;
-      axi_rresp <= 2'b00;
-    end else begin
-      if(axi_arready && S_AXI_ARVALID_I) begin
-        axi_rvalid <= 1'b1;
-        //axi_rdata <= GPIO_DATA_I;
-        axi_rresp <= 2'b00;
-      end else if(axi_rvalid && S_AXI_RREADY_I) begin
-        axi_rvalid <= 1'b0;
+  // Next-state logic
+  always_comb begin : fsm_read_next_state_logic
+    rd_state_next = 2'bx;
+
+    case (rd_state)
+      RD_IDLE: begin
+        if(s_axi_arvalid_i) begin
+          rd_state_next = RD_ADDR;
+        end else begin
+          rd_state_next = RD_IDLE;
+        end
       end
-    end
+
+      RD_ADDR: begin
+        rd_state_next = RD_WAIT_DONE;
+      end
+
+      RD_WAIT_DONE: begin
+        if(rd_done_i) begin
+          rd_state_next = RD_WRITE_AXI_SIG;
+        end else begin
+          rd_state_next = RD_WAIT_DONE;
+        end
+      end
+
+      RD_WRITE_AXI_SIG: begin
+        if(s_axi_rready_i) begin
+          rd_state_next = RD_IDLE;
+        end else begin
+          rd_state_next = RD_WRITE_AXI_SIG;
+        end
+      end
+
+      default: rd_state_next = RD_IDLE;
+    endcase
   end
-  assign axi_rdata = GPIO_DATA_I;
-endmodule
 
-  // typedef enum logic [1:0] {
-  //   RD_ADDR,
-  //   RD_DATA
-  // } read_state_t;
-  
-  // read_state_t rd_state;
-  // read_state_t rd_state_next;
-  
-  // // State register
-  // always_ff @(posedge S_AXI_ACLK or negedge S_AXI_ARESETN) begin : fsm_read_state_reg
-  //   if(!S_AXI_ARESETN) begin
-  //     rd_state <= RD_ADDR;
-  //   end else begin
-  //     rd_state <= rd_state_next;
-  //   end
-  // end
+  // Output register
+  always_ff @(posedge s_axi_aclk_i or negedge s_axi_areset_n) begin : fsm_out_reg
+    if(!s_axi_areset_n) begin
+      rd_en_o <= 1'b0;
+      rd_addr_o <= '0;
+      s_axi_rdata_o <= '0;
+      s_axi_rresp_o <= 2'b00;
+      s_axi_rvalid_o <= 1'b0;
+    end else begin
+      case (rd_state_next)
+        RD_IDLE: begin
+          rd_en_o <= 1'b0;
+          rd_addr_o <= '0;
+          s_axi_rdata_o <= '0;
+          s_axi_rresp_o <= 2'b00;
+          s_axi_rvalid_o <= 1'b0;
+        end
 
-  // // Next-state logic
-  // always_comb begin : fsm_read_next_state_logic
-  //   rd_state_next = 2'bx;
+        RD_ADDR: begin
+          rd_en_o <= 1'b1;
+          rd_addr_o <= s_axi_araddr_i;
+        end
 
-  //   case (rd_state)
-  //     RD_ADDR: begin
-  //       if(axi_arready && S_AXI_ARVALID_I) begin
-  //         rd_state_next = RD_DATA;
-  //       end else begin
-  //         rd_state_next = RD_ADDR;
-  //       end
-  //     end
+        RD_WAIT_DONE: begin
+          rd_en_o <= 1'b0;
+        end
 
-  //     RD_DATA: begin
-  //       if(axi_rvalid && S_AXI_RREADY_I) begin
-  //         rd_state_next = RD_ADDR;
-  //       end else begin
-  //         rd_state_next = RD_DATA;
-  //       end
-  //     end
-
-  //     default: rd_state_next = RD_ADDR;
-  //   endcase
-  // end
-
-  // output logic [AXI_ADDR_WIDTH-1:0]        READ_ADDR_O
-  // input  logic [AXI_DATA_WIDTH-1:0]        GPIO_DATA_I
-  // logic [AXI_ADDR_WIDTH-1:0]        read_addr;
-  // assign READ_ADDR_O = read_addr;
-  // // Output register
-  // always_ff @(posedge S_AXI_ACLK or negedge S_AXI_ARESETN) begin : fsm_out_reg
-  //   if(!S_AXI_ARESETN) begin
-  //     axi_arready <= 1'b1;
-  //     axi_rdata <= '0;
-  //     axi_rresp <= 2'b00;
-  //     axi_rvalid <= 1'b0;
-  //     read_addr <= '0;
-  //   end else begin
-
-  //     case (rd_state_next)
-  //       RD_ADDR: begin
-  //         axi_arready <= 1'b1;
-  //         if(axi_arready && S_AXI_ARVALID_I) begin
-  //           read_addr <= S_AXI_ARADDR_I;
-  //         end
-  //         if(axi_rvalid && S_AXI_RREADY_I) begin
-  //           axi_rvalid <= 1'b0;
-  //         end
-  //       end
-
-  //       RD_DATA: begin
-          
-  //       end
+        RD_WRITE_AXI_SIG: begin
+          s_axi_rvalid_o <= 1'b1;
+          s_axi_rresp_o <= rd_err_i;
+          s_axi_rdata_o <= rd_data_i;
+        end
       
-  //       default: rd_state_next = RD_ADDR; 
-  //     endcase
-  //   end
-  // end
-  // // ======================================
+      endcase
+    end
+  end
 
+  assign s_axi_arready_o = rd_done_i;
+
+endmodule
